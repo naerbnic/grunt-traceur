42877d2f385842c61e0a322f7d9407deb149e2ff
Brian Chin <naerbnic@ucla.edu>
2014-11-25 18:26:44 -0800

    Changed the compiler to use promises more generally for callbacks (except where absolutely necessary).

--- tasks/traceur.js
+++ tasks/traceur.js
@@ -15,59 +15,58 @@ var RUNTIME_PATH = (function () {
   return require('traceur').RUNTIME_PATH;
 })();
 
-function asyncCompile(content, options, callback) {
-  var result;
-  try {
-    result = compiler.compile(content, options);
-  } catch (e) {
-    callback(e.message, null);
-    return;
-  }
-  callback(null, result);
+function asyncCompile(content, options) {
+  return compiler.compile(content, options);
+}
+
+function assertPromise(b, msg) {
+  return new Promise(function(resolve, reject) {
+    if (b) {
+      resolve();
+    } else {
+      reject(Error(msg));
+    }
+  });
 }
 
 /*
 * Compiles one file
 */
 function compileOne (grunt, compile, src, dest, options) {
-  return new Promise(function (resolve, reject) {
-    if (src.length > 1) {
-      reject(Error('source MUST be a single file OR multiple files using expand:true. ' +
-        'Check out the README.'));
-    }
-    src = src[0];
-    var content = grunt.file.read(src).toString('utf8');
-    options.filename = src;
-    if (options.moduleNames) {
-      options.moduleName = [path.dirname(dest), path.sep, path.basename(dest, path.extname(dest))].join('');
-    }
-    compile(content, options, function (err, result) {
-      var sourceMapName, sourceMapPath;
-      if (err) {
-        grunt.log.error(src + ' -> ' + dest);
-        grunt.log.error('ERRORS:');
-        grunt.log.error(err);
-        reject(err);
-      } else {
-        if (options.includeRuntime) {
-          result[0] = fs.readFileSync(RUNTIME_PATH) + result[0];
-        }
-        if (options.sourceMaps) {
-          sourceMapName = path.basename(src) + '.map';
-          sourceMapPath = path.join(dest, '..',  sourceMapName);
-          result[0] += '//# sourceMappingURL=' + sourceMapName + '\n';
-          grunt.file.write(sourceMapPath, result[1]);
-          grunt.log.debug('SourceMap written to "' + sourceMapName + '"');
-        }
-        grunt.file.write(dest, result[0], {
-          encoding: 'utf8'
-        });
-        grunt.log.debug('Compiled successfully to "' + dest + '"');
-        grunt.log.ok(src + ' -> ' + dest);
-        resolve();
+  return assertPromise(src.length == 1,
+      'source MUST be a single file OR multiple files using expand:true. ' +
+      'Check out the README.')
+    .then(function() {
+      src = src[0];
+      var content = grunt.file.read(src).toString('utf8');
+      options.filename = src;
+      if (options.moduleNames) {
+        options.moduleName = [path.dirname(dest), path.sep, path.basename(dest, path.extname(dest))].join('');
       }
+      return compile(content, options);
+    })
+    .then(function(result) {
+      if (options.includeRuntime) {
+        result[0] = fs.readFileSync(RUNTIME_PATH) + result[0];
+      }
+      if (options.sourceMaps) {
+        var sourceMapName = path.basename(src) + '.map';
+        var sourceMapPath = path.join(dest, '..',  sourceMapName);
+        result[0] += '//# sourceMappingURL=' + sourceMapName + '\n';
+        grunt.file.write(sourceMapPath, result[1]);
+        grunt.log.debug('SourceMap written to "' + sourceMapName + '"');
+      }
+      grunt.file.write(dest, result[0], {
+        encoding: 'utf8'
+      });
+      grunt.log.debug('Compiled successfully to "' + dest + '"');
+      grunt.log.ok(src + ' -> ' + dest);
+    })
+    .catch(function(err) {
+      grunt.log.error(src + ' -> ' + dest);
+      grunt.log.error('ERRORS:');
+      grunt.log.error(err);
     });
-  });
 }
 
 module.exports = function(grunt) {
